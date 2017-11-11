183dbd8114f5fea057eb273826b078cb9ec7a757
nantongchenfei <31747978+nantongchenfei@users.noreply.github.com>
2017-11-11 17:55:13 -0500

    Add files via upload

--- TemplateMatching_ChenFei.py
+++ TemplateMatching_ChenFei.py
@@ -2,80 +2,58 @@ import numpy as np
 import cv2
 
 def TemplateMatching(src, temp, stepsize): # src: source image, temp: template image, stepsize: the step size for sliding the template
-    mean_t = np.uint64(0)
-    var_t = np.uint64(0)
+    src_data = np.uint64(src)
+    temp_data = np.uint64(temp)
+    mean_t = 0.0
+    var_t = 0.0
     location = [0, 0]
+    (H , W) = temp.shape
     # Calculate the mean and variance of template pixel values
-    # ------------------ Put your code below ------------------
-    sumSquare = np.uint64(0)
-    squareSum = np.uint64(0)
-    Sum = np.uint64(0)
-    N = temp.size
-    for i in np.arange(0, temp.shape[0]):
-        for j in np.arange(0, temp.shape[1]):
-            Sum += temp[i,j]
-            squareSum += (temp[i,j])**2
-    mean_t = Sum / N
-    var_t = (squareSum - Sum**2 / N)/N
+    N = H * W
+    mean_t = sum(sum(temp_data)) / N
+    var_t = sum(sum((temp_data - mean_t)**2)) / N
 
-    max_corr = 0;
     # Slide window in source image and find the maximum correlation
-    for i in np.arange(0, src.shape[0] - temp.shape[0], stepsize):
-        for j in np.arange(0, src.shape[1] - temp.shape[1], stepsize):
-            print ((i,j))
-            mean_s =  np.uint64(0)
-            var_s =  np.uint64(0)
-            corr =  np.uint64(0)
+    max_corr = 0;
+    for i in range(0, src.shape[0] - temp.shape[0], stepsize):
+        for j in range(0, src.shape[1] - temp.shape[1], stepsize):
+            mean_s =  0.0
+            var_s =  0.0
+            corr =  0.0
             # Calculate the mean and variance of source image pixel values inside window
-            # ------------------ Put your code below ------------------
-            sumSquare =  np.uint64(0)
-            squareSum =  np.uint64(0)
-            Sum = np.uint64(0)
-            for n in np.arange(0, temp.shape[0]):
-                for m in np.arange(0, temp.shape[1]):
-                    Sum += src[n+i,m+j]
-                    squareSum += (src[n+i,m+j])**2
-            mean_s = Sum / N
-            var_s = (squareSum - Sum**2 / N)/N
+            mean_s = sum(sum(src_data[i:i+H , j:j+W])) / N
+            var_s = (sum(sum((src_data[i:i+H , j:j+W] - mean_s)**2))) / N
             
             # Calculate normalized correlation coefficient (NCC) between source and template
-            # ------------------ Put your code below ------------------ 
-            for n in np.arange(0, temp.shape[0]):
-                for m in np.arange(0, temp.shape[1]):
-                    corr += (src[n+i,m+j] - mean_s) * (temp[n,m] - mean_t)
-            corr = corr / (N * var_t * var_s)
-            print("corr = " + str(corr))
+            corr = sum(sum((src_data[i:i+H , j:j+W] - mean_s) * (temp_data - mean_t))) / (N * var_t * var_s)
             if corr > max_corr:
                 max_corr = corr;
                 location = [i, j];
-                print("Max corr = " + str(corr))
     return location
 
 def main():
     # load source and template images
-    source_img = cv2.imread('source_img.jpg',0) # read image in grayscale
-    print(source_img.shape)
-    temp = cv2.imread('template.jpg',0) # read image in grayscale
-    print(temp.shape)
+    source_img = cv2.imread('source_img2.jpg',0) # read image in grayscale
+    temp = cv2.imread('template2.jpg',0) # read image in grayscale
+    
     location = TemplateMatching(source_img, temp, 20);
-    print(location)
     match_img = cv2.cvtColor(source_img, cv2.COLOR_GRAY2RGB)
 
     # Draw a red rectangle on match_img to show the template matching result
     # ------------------ Put your code below ------------------
-    (x, y) = location
+    (y, x) = location
     (h, w) = temp.shape
-    cv2.rectangle(match_img,(x,y),(x+w,y+h),(255,0,0),3)
+    cv2.rectangle(match_img,(x,y),(x+w,y+h),(0,0,255),2)
 
         
     # Save the template matching result image (match_img)
     # ------------------ Put your code below ------------------ 
     cv2.imwrite('MyTemplateMatching.jpg', match_img)
+##    cv2.imwrite('TemplateImage.jpg', temp)
 
 
     # Display the template image and the matching result
-    cv2.namedWindow('TemplateImage', cv2.WINDOW_NORMAL)
-    cv2.namedWindow('MyTemplateMatching', cv2.WINDOW_NORMAL)
+    cv2.imshow('sourceImage', source_img)
     cv2.imshow('TemplateImage', temp)
     cv2.imshow('MyTemplateMatching', match_img)
     cv2.waitKey(0)
